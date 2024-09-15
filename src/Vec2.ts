559afd45c6693fcc03afd2f7c57ad4d3350ac706
Garrett van Wageningen <ghvanw283@gmail.com>
2024-09-14 21:46:40 -0500

    initish

---
+++ src/Vec2.ts
@@ -0,0 +1,50 @@
+
+interface IVec2 {
+    x: number;
+    y: number;
+}
+
+class Vec2 implements IVec2 {
+
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    divideBy(n: number): Vec2 {
+        return new Vec2(this.x / n, this.y / n);
+    }
+
+    dot(other: IVec2): number {
+        return (this.x * other.x) + (this.y * other.y);
+    }
+
+    plus(other: IVec2): Vec2 {
+        return new Vec2(this.x + other.x, this.y + other.y);
+    }
+
+    magnitude(): number {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    minus(other: IVec2): Vec2 {
+        return new Vec2(this.x - other.x, this.y - other.y);
+    }
+
+    normalize(): Vec2 {
+        const mag = this.magnitude();
+        return this.divideBy(mag);
+    }
+
+    scaleBy(n: number): Vec2 {
+        return new Vec2(this.x * n, this.y * n);
+    }
+}
+
+export {
+    IVec2,
+    Vec2
+}
